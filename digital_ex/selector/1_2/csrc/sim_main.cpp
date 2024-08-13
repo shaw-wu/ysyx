59e0a501295d1f8a2a3198ae524ed880c42e6466
tracer-ysyx <tracer@ysyx.org>
2024-08-13 22:49:31 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:31 up 22:58,  5 users,  load average: 1.03, 1.14, 1.15

---
+++ digital_ex/selector/1_2/csrc/sim_main.cpp
@@ -0,0 +1,54 @@
+#include "Vtopp.h"
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated_vcd_c.h"
+#include <nvboard.h>
+
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+/*
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+*/
+
+int main(int argc, char** argv) {
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtopp* top = new Vtopp{contextp};
+  
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+ /* VerilatedVcdC* tfp = new VerilatedVcdC;
+	contextp->traceEverOn(true);
+	top->trace(tfp, 0);
+	tfp->open("wave.vcd");
+	*/
+//	while (!contextp->gotFinish()) {
+	while (1) {
+		/*int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+//		printf("a = %d, b = %d ,f = %d\n", a, b, top->f);
+		
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+
+		assert(top->f == (a ^ b));*/
+		//single_cycle();
+		dut.eval();
+		nvboard_update();
+	}
+	nvboard_quit();
+  delete top;
+	//tfp->close();
+  delete contextp;
+	return 0;
+}
