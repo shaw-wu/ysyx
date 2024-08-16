c17c3f4adb88d71fc44cda3e3944a35d1ab64b62
tracer-ysyx <tracer@ysyx.org>
2024-08-16 13:14:10 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:10 up 1 day, 14:33,  4 users,  load average: 0.84, 0.51, 0.37

--- digital_ex/decode/4_2/vsrc/encode.v
+++ digital_ex/decode/4_2/vsrc/encode.v
@@ -2,6 +2,6 @@ module encode(x, y);
 	input [3:0] x;
 	output [1:0] y;
 
-	assign y[0] = (~x[0] &  x[1] & ~x[2] & ~x[3]) + (~x[0] & ~x[1] & x[2] & ~x[3]);
-	assign y[1] = (~x[0] & ~x[1] & ~x[2] &  x[3]) + (~x[0] & ~x[1] & x[2] & ~x[3]);
+	assign y[0] = (~x[0] & ~x[1] & ~x[2] &  x[3]) + (~x[0] &  x[1] & ~x[2] & ~x[3]);
+	assign y[1] = (~x[0] & ~x[1] & ~x[2] &  x[3]) + (~x[0] & ~x[1] &  x[2] & ~x[3]);
 endmodule
