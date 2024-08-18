50a2fdeae07f3b0985128c395b994bdedb689785
tracer-ysyx <tracer@ysyx.org>
2024-08-18 19:44:56 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:56 up  2:07,  4 users,  load average: 1.05, 0.80, 0.67

---
+++ digital_ex/adder/add_sub/vsrc/add_sub_8b.v
@@ -0,0 +1,30 @@
+module complement (a, a_c);
+	input [7:0] a;
+	output [7:0] a_c;
+
+	wire [6:0] t_no_a;
+	assign t_no_a = {7{a[7]}} ^ a[6:0];
+	assign a_c = {a[7],t_no_a}; 
+endmodule
+
+module add_sub_8b (a, b, s_or_a, carry, zero, overflow, result);
+	input [7:0] a;
+  input [7:0] b;
+	input s_or_a;
+  output [7:0] result;
+	output carry,zero,overflow;
+
+	wire [7:0] a_c;
+	wire [7:0] b_c; 
+	wire [7:0] t_no_Cin;
+	complement i0(a[7:0], a_c[7:0]);
+	complement i1(b[7:0], b_c[7:0]);
+	assign t_no_Cin = {8{s_or_a}} ^ b;
+	assign {carry, result} = a + t_no_Cin + {7'b0000000,s_or_a};
+	assign overflow = (a[7] == b[7]) && (a[7] != result[7]);
+  assign zero = ~(| result);
+
+endmodule
+
+	
+
