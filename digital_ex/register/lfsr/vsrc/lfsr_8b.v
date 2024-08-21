23867d3861b3a3fd8bde95d2634d5fbc9c668f02
tracer-ysyx <tracer@ysyx.org>
2024-08-21 17:33:56 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:56 up 1 day,  4:52,  4 users,  load average: 0.19, 0.51, 0.49

--- digital_ex/register/lfsr/vsrc/lfsr_8b.v
+++ digital_ex/register/lfsr/vsrc/lfsr_8b.v
@@ -4,12 +4,13 @@ module lfsr_8b(in, clk, s, Q);
 	input s;
 	output reg [7:0] Q;
 
-	reg [7:0] q;
 	reg g;
 
 	always @(clk) begin
-			q = (in & {8{s}}) + (Q & {8{~s}});
-			g = q[4] ^ q[3] ^ q[2] ^ q[0];
-			Q = {g, q[7:1]};
+			if (s) Q = in;
+			else begin
+			  g = Q[4] ^ Q[3] ^ Q[2] ^ Q[0];
+				Q = {g, Q[7:1]};
+			end
 		end
 endmodule
