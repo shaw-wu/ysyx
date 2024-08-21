89185d24d01f7df521ac8dd4190744f00902048a
tracer-ysyx <tracer@ysyx.org>
2024-08-21 18:09:55 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:55 up 1 day,  5:28,  4 users,  load average: 1.14, 0.72, 0.61

--- digital_ex/register/shift_reg/vsrc/sr_4b.v
+++ digital_ex/register/shift_reg/vsrc/sr_4b.v
@@ -12,9 +12,9 @@ module sr_4b(in, clk, Q);
 	input clk;
 	output reg [3:0] Q;
 
-	Dflip i0(in  , clk, Q[3]);
-	Dflip i1(Q[3], clk, Q[2]);
-	Dflip i2(Q[2], clk, Q[1]);
-	Dflip i3(Q[1], clk, Q[0]);
+	Dflip i0(Q[1], clk, Q[0]);
+	Dflip i1(Q[2], clk, Q[1]);
+	Dflip i2(Q[3], clk, Q[2]);
+	Dflip i3(in  , clk, Q[3]);
 
 endmodule;
