4d0e6778513de929dc1dfc231848e00ad51eb1cc
tracer-ysyx <tracer@ysyx.org>
2024-08-23 16:34:07 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:07 up  6:51,  4 users,  load average: 0.67, 0.55, 0.46

--- digital_ex/register/lfsr/vsrc/lfsr_8b.v
+++ digital_ex/register/lfsr/vsrc/lfsr_8b.v
@@ -1,10 +1,49 @@
-module lfsr_8b(in, clk, s, Q);
+module decode(x, en, y);
+	input [3:0] x;
+	input en;
+	output reg [6:0] y;
+
+	always @(x or en) begin
+		if(en) begin
+			case(x)
+				4'b0000 : y = 7'b1000000;
+				4'b0001 : y = 7'b1111001;
+				4'b0010 : y = 7'b0100100;
+				4'b0011 : y = 7'b0110000;
+				4'b0100 : y = 7'b0011001;
+				4'b0101 : y = 7'b0010010;
+				4'b0110 : y = 7'b0000010;
+				4'b0111 : y = 7'b1111000;
+				4'b1000 : y = 7'b0000000;
+				4'b1001 : y = 7'b0010000;
+				4'b1010 : y = 7'b0001000;
+				4'b1011 : y = 7'b0000011;
+				4'b1100 : y = 7'b1000110;
+				4'b1101 : y = 7'b0100001;
+				4'b1110 : y = 7'b0000110;
+				4'b1111 : y = 7'b0001110;
+				default : y = 7'b1111111;
+			endcase
+		end
+		else y = 7'b1111111;
+	end
+endmodule
+
+module lfsr_8b(in, clk, s, Q, hex0, hex1);
 	input [7:0] in;
 	input clk;
 	input s;
 	output reg [7:0] Q;
+	output [6:0] hex0;
+	output [6:0] hex1;
 
 	reg g;
+	decode i0(.x  (in[3:0]),
+					  .en (1),
+						.y  (hex0));
+	decode i1(.x  (in[7:4]),
+					  .en (1),
+						.y  (hex1));
 
 	always @(posedge s or  posedge clk) begin
 			if (s) Q = in;
