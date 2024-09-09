c8c74538d11f9279392d80e47de57688b21a3404
tracer-ysyx <tracer@ysyx.org>
2024-09-09 13:07:22 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:07:22 up  3:24,  6 users,  load average: 0.68, 0.79, 0.82

--- digital_ex/state/keyb/vsrc/ps2_keyboard.v
+++ digital_ex/state/keyb/vsrc/ps2_keyboard.v
@@ -22,6 +22,7 @@ module ps2_keyboard(clk, clrn, ps2_clk, ps2_data, data, ready, nextdata_n, overf
 	always @(posedge clk) begin
 		//reset
 		if (clrn == 0) begin
+			$display("clrn : %h",clrn);
 			count <= 0;
 			w_ptr <= 0; r_ptr <= 0;
 		  overflow <= 0;
