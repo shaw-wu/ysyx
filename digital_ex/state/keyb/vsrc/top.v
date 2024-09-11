4d1a1d9cd29b6285c95753a4af187b0787a83cdc
tracer-ysyx <tracer@ysyx.org>
2024-09-11 18:21:21 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:21 up  4:18,  7 users,  load average: 0.55, 0.87, 1.03

--- digital_ex/state/keyb/vsrc/top.v
+++ digital_ex/state/keyb/vsrc/top.v
@@ -65,12 +65,12 @@ kbd7seg seg (
          8'b00111110,4'd3,4'd8 ,  
          8'b01000110,4'd3,4'd9 ,  
          8'b01000101,4'd3,4'd0 }), 
-	.hex0(hex0In),
-	.hex1(hex1In),
-	.hex2(hex2In),
-	.hex3(hex3In)
+	.hex0(hex0),
+	.hex1(hex1),
+	.hex2(hex2),
+	.hex3(hex3)
 );
-
+/*
 handle_hex hdl_h0(
 	.clk(clk),
 	.cond(break_code),
@@ -95,7 +95,7 @@ handle_hex hdl_h3(
 	.hex_in(hex3In),
 	.hex_out(hex3)
 );
-
+*/
 handle hdl (
 	.clk(clk),
 	.ready(ready),
