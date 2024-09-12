5c44151e64770faf4ce47369c83a9d309dd1a3a4
tracer-ysyx <tracer@ysyx.org>
2024-09-12 21:19:57 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:57 up 1 day,  7:16,  7 users,  load average: 1.06, 1.16, 1.26

--- digital_ex/state/keyb/vsrc/ps2_keyboard.v
+++ digital_ex/state/keyb/vsrc/ps2_keyboard.v
@@ -60,12 +60,12 @@ module ps2_keyboard(
 				end
 			end
 		end
-		if (fifo[r_ptr-2] == 8'hF0 || fifo[r_ptr] == 8'hF0) begin
+		if (fifo[r_ptr-2] == 8'hF0) begin
 			break_code <= 1;
 		end else begin
 			break_code <= 0;
 		end
-		//$display("data : %h",fifo[r_ptr]);
+		$display("fifo : %h",fifo);
 	end
 	assign data = fifo[r_ptr-1];
 endmodule
