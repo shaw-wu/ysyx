829cd53b751b511c3588800ca8da6e0549cfb6c0
tracer-ysyx <tracer@ysyx.org>
2024-09-12 21:31:54 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:54 up 1 day,  7:28,  7 users,  load average: 1.53, 1.29, 1.29

--- digital_ex/state/keyb/vsrc/handle_hex.v
+++ digital_ex/state/keyb/vsrc/handle_hex.v
@@ -8,6 +8,7 @@ module handle_hex(
 always @(posedge clk) begin
 	if (!cond) begin
 		hex_out = hex_in;
+		$display("0");
 	end else begin
 		hex_out = 7'b111_1111;
 		$display("break_code : %d",cond);
