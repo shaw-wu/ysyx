3b3a3a5319a5b286d22570c2fd37f21c8ff3d81d
tracer-ysyx <tracer@ysyx.org>
2024-09-12 18:48:30 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:48:30 up 1 day,  4:45,  7 users,  load average: 1.41, 1.16, 1.15

--- digital_ex/state/keyb/vsrc/handle.v
+++ digital_ex/state/keyb/vsrc/handle.v
@@ -7,9 +7,11 @@ module handle(
 	output reg nextdata_n
 );
 
-always @(posedge clk,ready) begin
+always @(posedge clk) begin
+	if (ready) begin
 		dataTemp = data;
 		$display("dataTemp : %h, data : %h",dataTemp,data);
+	end
 end
 delayed_assignment delay (
 		.clk(clk),
