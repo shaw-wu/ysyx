e88d8c573c10812208e3492e409008c8514e6af1
tracer-ysyx <tracer@ysyx.org>
2024-12-04 16:53:49 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:49 up  5:45,  5 users,  load average: 1.61, 1.59, 1.57

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,18 +24,20 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-	//int n = sizeof(regs);
-	//int row = n / 4 + 1;
+	printf("%s,%s,%s,%s\n",regs[0],regs[1],regs[2],regs[3]);
+	printf("%d,%d,%d,%d\n",*regs[0],*regs[1],*regs[2],*regs[3]);
+	/*
+	int n = sizeof(regs);
+	int row = n / 4 + 1;
 	int i, j;
 	i = 0;
-	for(j = 0; j < 1; j++){
-		for(; i < 1; i++){
-			//printf("1 \n");
+	for(j = 0; j < row; j++){
+		for(; i < n; i++){
 			printf("%s : %d ", reg_name(i), *regs[i]);
-			//printf("1 ");
 		}
 		printf("\n");
 	}
+	*/
 	return;
 }
 
