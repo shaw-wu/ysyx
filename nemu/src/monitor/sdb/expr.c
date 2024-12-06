a4cb8a01fc25e16b89bae76d3fb6890e0b53e0b1
tracer-ysyx <tracer@ysyx.org>
2024-12-06 19:07:49 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:49 up  3:37,  4 users,  load average: 0.87, 0.67, 0.50

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,7 +103,6 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 				/*wxz*/
-				char* end_flag = '\000';
         switch (rules[i].token_type) {
           case TK_NOTYPE: 
 						break;
@@ -111,7 +110,7 @@ static bool make_token(char *e) {
 						tokens[nr_token].type = TK_LBRKET;
 						assert(substr_len < 32);
 					  strncpy(tokens[nr_token].str, substr_start, substr_len);
-					  strcat(end_flag, tokens[nr_token].str+substr_len); 
+						tokens[nr_token].str[substr_len] = '\0';
 						nr_token++;
 						break;
           case TK_RBRKET : 
