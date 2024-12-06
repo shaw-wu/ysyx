b5f9e04655aee6dc932960cd6cfc4fd6114047d6
tracer-ysyx <tracer@ysyx.org>
2024-12-06 18:35:23 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:35:23 up  3:05,  4 users,  load average: 0.11, 0.30, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -170,7 +170,11 @@ static bool make_token(char *e) {
 	printf("nr_token: %d\n",nr_token);
 	printf("position: %d\n",position);
 	for(int k = 0; k < nr_token; k++){
-		printf("%s(%d)",tokens[k].str,tokens[k].type);
+		printf("%s)",tokens[k].str);
+	}
+	printf("\n");
+	for(int k = 0; k < nr_token; k++){
+		printf("(%d)",tokens[k].type);
 	}
 	printf("\n");
 
