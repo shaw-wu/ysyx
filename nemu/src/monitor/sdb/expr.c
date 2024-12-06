f1100b51a44e7b83f9e2b57495852f5796700e9f
tracer-ysyx <tracer@ysyx.org>
2024-12-06 17:23:46 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:46 up  1:53,  4 users,  load average: 0.52, 0.35, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -46,7 +46,7 @@ static struct rule {
   {"\\+" , '+'},         // plus
   {"-"   , '-'},         // minus
   {"=="  , TK_EQ},       // equal
-  {"\\d" , TK_DIG},      // digital 
+  {"\\[:difital:\\]" , TK_DIG},      // digital 
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -159,12 +159,12 @@ static bool make_token(char *e) {
       return false;
     }
   }
-/*	
+
 	for(int k = 0; k < nr_token; k++){
 		printf("%s(%d)",tokens[k].str,tokens[k].type);
 	}
 	printf("\n");
-*/
+
   return true;
 }
 
