766645786307b36f15f78a2c7e38b7fbb27131d1
tracer-ysyx <tracer@ysyx.org>
2025-01-08 15:10:50 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:10:50 up 21:15, 13 users,  load average: 3.17, 3.20, 2.97

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -27,13 +27,13 @@ static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned int result = %s; "
+"  uint32_t result = %s; "
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
 
 static void gen_rand_expr() {
-	unsigned int rand1 = rand() % 8;
+	uint32_t rand1 = rand() % 8;
 	int ind_st1, ind_st2;
 	int i;
 	switch(rand1){
@@ -42,7 +42,7 @@ static void gen_rand_expr() {
 		case 2 :
 		case 3 :
 		case 4 :
-			unsigned int d = rand() % 2 + 1;
+			uint32_t d = rand() % 2 + 1;
 			for(i = 0; i < d; i++){
 				if(i == 0){
 					buf[buf_i] = (char)(rand() % 9 + 1 + 48);//首位不为0
@@ -85,7 +85,7 @@ static void gen_rand_expr() {
 			gen_rand_expr();
 			int devision = 0;
 			int Zero = 0;
-			unsigned int rand2 = rand() % 4;
+			uint32_t rand2 = rand() % 4;
 			switch(rand2){
 				case 0 : 
 					buf[buf_i] = '+';
@@ -142,7 +142,7 @@ static void gen_rand_expr() {
 					fp = popen("/tmp/.expr", "r");											 /*打开进程*/
 					assert(fp != NULL);
 
-					unsigned int result;
+					uint32_t result;
 					ret = fscanf(fp, "%u", &result);										 /*读取程序结果*/
 					pclose(fp);
 
@@ -190,7 +190,7 @@ int main(int argc, char *argv[]) {
     fp = popen("/tmp/.expr", "r");											 /*打开进程*/
     assert(fp != NULL);
 
-    unsigned int result;
+    uint32_t result;
     ret = fscanf(fp, "%u", &result);										 /*读取程序结果*/
     pclose(fp);
 
