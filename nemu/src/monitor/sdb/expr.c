fef27582153b7b7d38d92185b06cae00ed4dd841
tracer-ysyx <tracer@ysyx.org>
2025-01-08 21:09:25 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:25 up 1 day,  3:14, 13 users,  load average: 2.93, 2.99, 2.77

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -360,7 +360,7 @@ static uint32_t eval(int st, int en, bool* illegal){
 				val1 = val1 & 0x7fffffff;
 				val2 = val2 & 0x7fffffff;
 				multi_res = val1 * val2;
-				res = multi_res & 0x00000000ffffffff;
+				res = multi_res & 0x000000007fffffff;
 				if(multi_res >= 0xffffffff){
 					overflow = 1;
 				}
