380a891e5ef503668d979d606ea0164b55d18bd1
tracer-ysyx <tracer@ysyx.org>
2025-01-09 10:43:52 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:43:52 up 1 day, 16:48, 14 users,  load average: 2.22, 2.45, 2.46

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -185,10 +185,6 @@ static int cmd_et(char *args) {
 	assert(fp != NULL);
 	char line[65536 + 16] = {};
 	while(fgets(line, sizeof(line), fp) != NULL){
-		char *newline = strtok(line, "\n");
-		if(newline != NULL){
-			strcpy(line, newline);
-		}
 	  char ex[65536] = {};
 		memset(ex, '\0', 65536);
 		char* result = strtok(line, " ");
@@ -198,10 +194,10 @@ static int cmd_et(char *args) {
 		sscanf(result, "%u", &rt);
 		expr(ex, &suc, &rp);
 		if(rp == rt){
-			fprintf(log, "%u = %s\n", rp, ex);
+			fprintf(log, "%u = %s", rp, ex);
 		}
 		else{
-			fprintf(log, "%u != %s(= %u)\n", rp, ex, rt);
+			fprintf(log, "(=%u)%u != %s\n",rt,  rp, ex);
 		}
 	}
 	fclose(fp);
