218547f4be4307f888f2e830c55edd1201a58095
tracer-ysyx <tracer@ysyx.org>
2025-01-11 14:54:08 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:08 up 3 days, 20:59, 18 users,  load average: 1.61, 1.55, 1.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -307,8 +307,6 @@ void sdb_mainloop() {
     cmd_c(NULL);
     return;
   }
-	int *lk = NULL;
-	printf("%d", *lk);
 
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
