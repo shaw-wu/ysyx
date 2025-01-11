86e53cef49ec74dea6e439fa1e85fb1ae65bfde5
tracer-ysyx <tracer@ysyx.org>
2025-01-11 19:20:18 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:18 up 4 days,  1:25, 18 users,  load average: 1.40, 1.55, 1.68

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -211,7 +211,7 @@ static int cmd_x(char *args) {
 /*wxz*/
 static int cmd_etn(char *args) {
 	//char* arg = strtok(NULL," ");
-	char *arg = args + 4;
+	char *arg = args;
 	char ex[65536] = {};
 	memset(ex, '\0',  65536);
 	strcpy(ex, arg);
