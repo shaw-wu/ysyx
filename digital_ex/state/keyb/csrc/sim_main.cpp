628e189822c90f38a8690cec2df10efe0aa77beb
tracer-ysyx <tracer@ysyx.org>
2025-01-11 21:08:04 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:04 up 4 days,  3:12, 18 users,  load average: 2.50, 2.29, 2.12

--- digital_ex/state/keyb/csrc/sim_main.cpp
+++ digital_ex/state/keyb/csrc/sim_main.cpp
@@ -3,19 +3,22 @@
 #include <verilated_vcd_c.h>
 #include <nvboard.h>
 
-//VerilatedContext* contextp = NULL; // 声明上下文变量
-//VerilatedVcdC* tfp = NULL;         // 声明波形变量
+#define CONFIG_NVBOARD
+//#define CONFIG_WAVE
 
-//static Vtop* top;               // 声明模块变量
-static TOP_NAME dut;
-void nvboard_bind_all_pins(TOP_NAME* top);
+VerilatedContext* contextp = NULL; // 声明上下文变量
+static Vtop* top;               // 声明模块变量
+VerilatedVcdC* tfp = NULL;         // 声明波形变量
 
 //波形时间
-//void step_and_dump_wave(){
-//	top->eval();                     // 更新电路状态
-//	contextp->timeInc(1);            // 时间宽度设为1
-//	tfp->dump(contextp->time());     // 波形时间宽度设置
-//}
+void step_and_dump_wave(){
+	top->eval();                     // 更新电路状态
+	contextp->timeInc(1);            // 时间宽度设为1
+	tfp->dump(contextp->time());     // 波形时间宽度设置
+}
+
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 //  单周期时钟
 static void single_cycle() {
@@ -30,37 +33,52 @@ static void reset(int n) {
 	dut.clrn = 1;
 }
 
-///初始化仿真
 void sim_init(int argc, char** argv ){
-//	contextp = new VerilatedContext;  // 初始化
-//	tfp = new VerilatedVcdC;
-//	contextp->commandArgs(argc, argv);
-//	top = new Vtop;                 // 实例化模块
+#ifdef CONFIG_NVBOARD
 	nvboard_bind_all_pins(&dut);
 	nvboard_init();
 	reset(10);
-//	contextp->traceEverOn(true);      // 打开波形跟踪
-//	top->trace(tfp, 0);               // 链接跟踪变量tfp与实例化模块top
-//	tfp->open("dump.vcd");            // 创建文件
+#endif
+
+#ifdef CONFIG_VERILATOR
+	contextp = new VerilatedContext;  // 初始化
+	tfp = new VerilatedVcdC;
+	contextp->commandArgs(argc, argv);
+	top = new Vtop;                 // 实例化模块
+#endif
+
+#ifdef CONFIG_WAVE
+	contextp->traceEverOn(true);      // 打开波形跟踪
+	top->trace(tfp, 0);               // 链接跟踪变量tfp与实例化模块top
+	tfp->open("dump.vcd");            // 创建文件
+#endif
 }
 
 // 退出仿真
-//void sim_exit(){
-//	step_and_dump_wave();   //
-//	tfp->close();           // 关闭tfp
-//}
+void sim_exit(){
+/*#ifdef CONFIG_WAVE
+	step_and_dump_wave();   //
+#endif*/
+	tfp->close();           // 关闭tfp
+}
 
 
 int main(int argc, char** argv) {
 	sim_init(argc, argv);
 	while(1){
+#ifdef CONFIG_NVBOARD
 		nvboard_update();
 		single_cycle();
+#endif
 	//  dut.eval();
-	//	step_and_dump_wave();
+#ifdef CONFIG_WAVE
+		step_and_dump_wave();
+#endif
 	}
+#ifdef CONFIG_NVBOARD
 	nvboard_quit();
-	//delete top;
-	//delete contextp;
+#endif
+	delete top;
+	delete contextp;
 	return 0;
 }
