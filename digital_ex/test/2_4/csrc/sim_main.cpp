24dee06edf49361790943ef6339d7eb7944d45ac
tracer-ysyx <tracer@ysyx.org>
2025-01-12 21:35:23 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:23 up 5 days,  3:40,  6 users,  load average: 2.40, 2.41, 2.46

--- digital_ex/test/2_4/csrc/sim_main.cpp
+++ digital_ex/test/2_4/csrc/sim_main.cpp
@@ -1,70 +1,36 @@
 #include "Vtestbench.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include <nvboard.h>
-
-#define CONFIG_WAVETRACE
-//#define CONFIG_NVBOARD
 
 VerilatedContext* contextp = NULL; // 上下文变量
 VerilatedVcdC* tfp = NULL;         // 波形变量
 static Vtestbench* top;               // 声明模块变量
 static int sim_time = 500000;
-static TOP_NAME dut;
-
-//nvboard的clk驱动信号
-static void single_cycle() {
-	dut.clk = 0; dut.eval();
-	dut.clk = 1; dut.eval();
-}
-//nvboard的reset驱动信号
-static void reset(int n){
-	dut.rst = 1;
-	while (n --> 0) single_cycle();
-	sut.rst = 0;
-}
 
 void sim_init(int argc, char** argv ){
-#ifndef CONFIG_NVBOARD
 	contextp = new VerilatedContext;  
 	contextp->commandArgs(argc, argv);
 	top = new Vtestbench;                 
-	#ifdef CONFIG_WAVETRACE
+	#ifdef ENABLE_WAVEFORM
 		Verilated::traceEverOn(true);
 		tfp = new VerilatedVcdC;
 		top->trace(tfp, 99);
 		tfp->open("build/dump.vcd");
 	#endif
-#else
-	nvboard_bind_all_pins(&dut);
-	nvboard_init();
-	reset(10);
-#endif
 }
 
 void main_loop(){
-#ifndef CONFIG_NVBOARD
 	while(contextp->time() < sim_time && !contextp->gotFinish()){
 		contextp->timeInc(1);
 		top->eval();
-	#ifdef CONFIG_WAVETRACE
+	#ifdef ENABLE_WAVEFORM
 		tfp->dump(contextp->time());
 	#endif
 	}
-#else
-	while(1) {
-		nvboard_update();
-		single_cycle();
-	}
-#endif
 }
 
 void sim_exit(){
-#ifdef CONFIG_NVBOARD
-	printf("Quit nvboard!\n");
-	return;
-#endif
-#ifdef CONFIG_WAVETRACE
+#ifdef ENABLE_WAVEFORM
 	if(tfp){
 		tfp->close();
 		delete tfp;
