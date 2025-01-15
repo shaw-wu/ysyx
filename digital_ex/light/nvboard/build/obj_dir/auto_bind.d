904f2fc8c5441f1fca566acae364f2497dd789cc
shaw <xz_640888@163.com>
2025-01-15 09:34:19 +0800

    Suplement missing files

---
+++ digital_ex/light/nvboard/build/obj_dir/auto_bind.d
@@ -0,0 +1,9 @@
+auto_bind.o: \
+ /home/shaw/ysyx-workbench/digital_ex/light/nvboard/build/auto_bind.cpp \
+ /home/shaw/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/shaw/ysyx-workbench/nvboard/usr/include/pins.h Vlight.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h
---
+++ digital_ex/light/nvboard/build/obj_dir/main.d
@@ -0,0 +1,8 @@
+main.o: /home/shaw/ysyx-workbench/digital_ex/light/nvboard/csrc/main.cpp \
+ /home/shaw/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/shaw/ysyx-workbench/nvboard/usr/include/pins.h Vlight.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h
