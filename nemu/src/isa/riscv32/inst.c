28a420c7a0df5f174b0b5ae6dbdeeb87d2ebd50c
tracer-ysyx <tracer@ysyx.org>
2025-02-18 18:55:47 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:47 up 20:14,  7 users,  load average: 1.02, 0.88, 0.88

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -21,7 +21,7 @@
 #define R(i) gpr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
-#define JMPO(x) s->dnpc += (x - 4)
+#define JMPO(x) s->dnpc += (uint32_t)((int32_t)x - (int32_t)4)
 #define JMP(x) s->dnpc = x
 
 enum {
