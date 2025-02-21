03c6ce8ca068c26b5c6297585c2256824ec3b156
tracer-ysyx <tracer@ysyx.org>
2025-02-21 16:52:53 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:53 up 59 min,  4 users,  load average: 2.52, 1.19, 0.68

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -51,6 +51,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_R: src1R(); src2R()				 ; break;
   }
 	if(type == TYPE_B){
+		printf("%x\n",i);
 		printf("%u\n",*imm);
 	}
 	assert(1);
