35b66981d7c06b59c650891537be25eb0b4f75cf
tracer-ysyx <tracer@ysyx.org>
2025-08-30 11:17:44 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:44 up 14:11,  6 users,  load average: 1.43, 1.61, 1.41

--- npc/sim/irom/irom.cpp
+++ npc/sim/irom/irom.cpp
@@ -6,7 +6,9 @@ static uint32_t ROM[ROM_SIZE];
 void load_rom(FILE *fp){
 	char line[8] = {};
 	int i;
+	printf("0\n");
 	while(fgets(line, sizeof(line), fp)){
+		printf("1\n");
 		uint32_t inst;
 		sscanf(line, "%8x", &inst);
 		ROM[i++] = inst;
@@ -15,6 +17,7 @@ void load_rom(FILE *fp){
 }
 
 uint32_t read_irom(uint32_t vaddr){
+	printf("2\n");
 	uint32_t paddr = vaddr >> 2;	
 	return ROM[paddr];
 }
