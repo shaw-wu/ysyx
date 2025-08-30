9a7d79e58b4789244999a7595254cbbd8d17e3c6
tracer-ysyx <tracer@ysyx.org>
2025-08-30 15:39:58 +0800

    > sim RTL ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:58 up 18:33,  8 users,  load average: 2.33, 2.51, 2.59

--- npc/sim/ebreak.cpp
+++ npc/sim/ebreak.cpp
@@ -1,8 +1,10 @@
 #include <svdpi.h>
 #include <Vysyx_25010009_top__Dpi.h>
 #include <ebreak.h>
+#include <cassert>
 
 void is_ebreak(int ebreak){
+	assert(0);
   if(ebreak) stop_sim = 1; 
 	return;
 }
