fee759e636842be405b410d19687c17985ddc106
tracer-ysyx <tracer@ysyx.org>
2025-09-01 17:48:18 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:18 up 2 days, 20:42,  6 users,  load average: 2.57, 2.49, 2.39

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -31,7 +31,7 @@ extern int ptr;
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-			for(int i = 0; i < ptr; i++) log_write("%s\n", iringbuf[i]); \
+			for(int i = 0; i < ptr; i++) printf("%s\n", iringbuf[i]); \
       MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
         (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__))); \
       IFNDEF(CONFIG_TARGET_AM, extern FILE* log_fp; fflush(log_fp)); \
