cd9ceb154719217db961e9df8bec02e93d04ba7c
tracer-ysyx <tracer@ysyx.org>
2025-09-01 20:45:19 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:19 up 2 days, 23:39,  8 users,  load average: 2.39, 2.37, 2.31

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -30,7 +30,6 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
-	printf("read len:%d\n",len);
 #ifdef CONFIG_MTRACE
 	switch(len){
 		case 1: printf("[pmem_read] addr: 0x%08x   rdata: 0x%02x\n", addr, ret);break;
