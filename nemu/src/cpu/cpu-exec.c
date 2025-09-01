f188ad9bb0268d33fa6aed30ff4a038fb9298737
tracer-ysyx <tracer@ysyx.org>
2025-09-01 17:52:05 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:05 up 2 days, 20:45,  6 users,  load average: 2.34, 2.40, 2.37

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -104,7 +104,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
 //	int plen = p - s->logbuf;
 	char tmp[128] = {};
 	sprintf(tmp, "%s", s->logbuf);
-	strcpy(iringbuf[ptr], tmp);
+	printf("tmp:%s, s->logbuf:%s\n", tmp, s->logbuf);
+	strcpy(iringbuf[ptr++], tmp);
 	ptr = ptr % 32;
 //	if(error_){
 //		for(i = 0; i < ptr; i++) log_write("%s\n", iringbuf[i]);
