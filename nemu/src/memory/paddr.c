3b7d9ab404397acadbcd605243481289e055d0d3
tracer-ysyx <tracer@ysyx.org>
2025-09-01 20:40:28 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:40:28 up 2 days, 23:34,  8 users,  load average: 2.10, 2.28, 2.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -48,7 +48,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 		case 1: printf("[pmem_write] addr: 0x%08x   wdata: 0x%02x\n", addr, data);
 		case 2: printf("[pmem_write] addr: 0x%08x   wdata: 0x%04x\n", addr, data);
 		case 4: printf("[pmem_write] addr: 0x%08x   wdata: 0x%08x\n", addr, data);
-    IFDEF(CONFIG_ISA64, case 8: printf("[pmem_read] addr: 0x%08x   rdata: 0x%016x\n", addr, data));
+    IFDEF(CONFIG_ISA64, case 8: printf("[pmem_write] addr: 0x%08x   rdata: 0x%016x\n", addr, data));
     default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
 	}
 #endif
