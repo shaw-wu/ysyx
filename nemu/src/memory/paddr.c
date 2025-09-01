4ed4482823cb12896b48960d71d63df6f384f69c
tracer-ysyx <tracer@ysyx.org>
2025-09-01 21:14:36 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:36 up 3 days, 8 min,  8 users,  load average: 2.33, 2.39, 2.37

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -37,10 +37,10 @@ static word_t pmem_read(paddr_t addr, int len) {
 		is_ifetch = 0;
 	} else {
 		switch(len){
-			case 1: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) "addr: 0x%08x   rdata: 0x%02x\n", addr, ret);break;
-			case 2: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) "addr: 0x%08x   rdata: 0x%04x\n", addr, ret);break;
-			case 4: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) "addr: 0x%08x   rdata: 0x%08x\n", addr, ret);break;
-    	IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) "addr: 0x%08x   rdata: 0x%016x\n", addr, ret));break;
+			case 1: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%02x\n", addr, ret);break;
+			case 2: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%04x\n", addr, ret);break;
+			case 4: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%08x\n", addr, ret);break;
+    	IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%016x\n", addr, ret));break;
     	default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);break;
 		}
 	}
@@ -51,10 +51,10 @@ static word_t pmem_read(paddr_t addr, int len) {
 static void pmem_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
 	switch(len){
-		case 1: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) "addr: 0x%08x   wdata: 0x%02x\n", addr, data);break;
-		case 2: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) "addr: 0x%08x   wdata: 0x%04x\n", addr, data);break;
-		case 4: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) "addr: 0x%08x   wdata: 0x%08x\n", addr, data);break;
-    IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) "addr: 0x%08x   wdata: 0x%016x\n", addr, data));break;
+		case 1: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%02x\n", addr, data);break;
+		case 2: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%04x\n", addr, data);break;
+		case 4: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%08x\n", addr, data);break;
+    IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%016x\n", addr, data));break;
     default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);break;
 	}
 #endif
