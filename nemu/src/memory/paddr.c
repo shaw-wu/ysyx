5e49fae75a48cdffe860a51b34c4b09bb0b8c084
shaw <xz_640888@163.com>
2025-09-01 21:16:09 +0800

    add mtrace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,11 +18,13 @@
 #include <device/mmio.h>
 #include <isa.h>
 
-extern int error_;
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+#endif
+#ifdef CONFIG_MTRACE
+extern int is_ifetch;
 #endif
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
@@ -30,15 +32,36 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
+#ifdef CONFIG_MTRACE
+	if(is_ifetch){
+		is_ifetch = 0;
+	} else {
+		switch(len){
+			case 1: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%02x\n", addr, ret);break;
+			case 2: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%04x\n", addr, ret);break;
+			case 4: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%08x\n", addr, ret);break;
+    	IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_read ]", ANSI_FG_CYAN) " addr: 0x%08x   rdata: 0x%016x\n", addr, ret));break;
+    	default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);break;
+		}
+	}
+#endif
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+	switch(len){
+		case 1: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%02x\n", addr, data);break;
+		case 2: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%04x\n", addr, data);break;
+		case 4: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%08x\n", addr, data);break;
+    IFDEF(CONFIG_ISA64, case 8: printf(ANSI_FMT("[pmem_write]", ANSI_FG_CYAN) " addr: 0x%08x   wdata: 0x%016x\n", addr, data));break;
+    default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);break;
+	}
+#endif
   host_write(guest_to_host(addr), len, data);
 }
 
 static void out_of_bound(paddr_t addr) {
-	error_ = 1;
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
