d717ed9cf004984c88b5300b13c6af932761366a
tracer-ysyx <tracer@ysyx.org>
2025-09-01 19:41:38 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:41:38 up 2 days, 22:35,  8 users,  load average: 2.56, 2.56, 2.40

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -21,6 +21,29 @@
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
+  0x00000013,  // nop(addi zero, 0)
 	0x01100313,  // addi t1, zero, 17
 	0x406282b3,  // sub t0, t0, t1
   0x00028823,  // sb  zero,16(t0)
