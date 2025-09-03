ed2a97dee227cbc660bca5cd1c65c302a2caea72
tracer-ysyx <tracer@ysyx.org>
2025-09-03 16:20:32 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:32 up 22:22, 13 users,  load average: 1.07, 0.92, 0.63

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,15 +82,17 @@ static int parse_args(int argc, char *argv[]) {
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+				printf("\n%s\n", argv[optind]);
+				printf("\n%s\n", argv[optind+1]);
+				printf("\n%s\n", argv[optind+2]);
+				printf("\n%s\n", argv[optind+3]);
+				printf("\n%s\n", argv[optind+4]);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1: 
-				printf("\n%s\n", argv[optind]);
-				printf("\n%s\n", argv[optind+1]);
-				printf("\n%s\n", argv[optind+2]);
 				img_file = argv[optind]; FTRACE_ARGS; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
