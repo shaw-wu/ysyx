e2575e2ff1e504b066e22b09e052473fb3cd6e45
shaw <xz_640888@163.com>
2025-09-03 12:18:45 +0800

    ftrace adding

--- nemu/tools/ftrace/ftrace.h
+++ nemu/tools/ftrace/ftrace.h
@@ -1,12 +1,18 @@
 #ifndef __FTRACE_H__
 #define __FTRACE_H__
 
+#include <stdint.h>
+#include <stdbool.h>
 #include <isa.h>
 
-extern FILE* elf_file;
+extern char* elf_file;
 extern bool have_img;
 extern bool is_call;
 extern bool is_ret;
 extern vaddr_t dnpc;
+
+extern Elf32_Sym* symtab = NULL; 
+extern char **sym_name = NULL;
+extern int sym_count = 0; 
 
 #endif
