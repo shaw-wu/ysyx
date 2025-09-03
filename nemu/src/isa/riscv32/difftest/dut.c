5c37e6861bac164497ce813cd7f73eac44ddce76
tracer-ysyx <tracer@ysyx.org>
2025-09-03 22:25:24 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:24 up 1 day,  4:27, 16 users,  load average: 0.21, 0.24, 0.26

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,6 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+	printf("dif\n");
 	while(1){
 		if(ref_r->pc != cpu.pc) break;
 		for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++){
