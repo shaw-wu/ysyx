b132693a2af8061f08d497acc44eb7898d06e7a3
tracer-ysyx <tracer@ysyx.org>
2025-09-03 18:37:30 +0800

    > compile NEMU ysyx_202301202 吴孝洲 Linux shaw 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:37:30 up 1 day, 39 min, 12 users,  load average: 0.21, 0.30, 0.29

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -11,11 +11,10 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt 
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt -b
 
 IMG := $(IMAGE).bin
-IMG_ELF := $(IMAGE).elf
-IMG_ELF_WITH_OPT := "-e $(IMG_ELF)"
+IMG_ELF := -e $(IMAGE).elf
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
 .PHONY: $(AM_HOME)/am/src/platform/nemu/trm.c
@@ -26,7 +25,7 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" ELF=$(IMG_ELF_WITH_OPT) IMG=$(IMG)
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" ELF="$(IMG_ELF)" IMG=$(IMG)
 
 gdb: image
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" ELF=$(IMG_ELF_WITH_OPT) IMG=$(IMG) 
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" ELF="$(IMG_ELF)" IMG=$(IMG) 
