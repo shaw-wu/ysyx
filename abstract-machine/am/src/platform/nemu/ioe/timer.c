d221af7dcdaaed6dbfffb7b8cca0814063005e57
shaw <xz_640888@163.com>
2025-09-11 23:25:13 +0800

    serial and timer

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,11 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+	uint32_t uptime_l, uptime_h;
+	uptime_h = inl(RTC_ADDR+4);
+	uptime_l = inl(RTC_ADDR);
+	//printf("uptime_h = %x, uptime_l = %x\n", uptime_h, uptime_l);
+	uptime->us = ((uint64_t)uptime_l) | ((uint64_t)uptime_h << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -31,8 +31,70 @@ int int2str(int value, char *buf) {
   return i;
 }
 
+int x2str(uint32_t value, char *buf) {
+  char *p = buf;
+  int i = 0;
+	uint32_t mod;
+  do {
+		mod = value % 16;
+    if(mod < 10) p[i++] = '0' + mod;
+		else         p[i++] = 'a' + mod - 10;
+    value /= 16;
+  } while (value > 0);
+
+  for (int j = 0; j < i / 2; j++) {
+    char tmp = p[j];
+    p[j] = p[i - 1 - j];
+    p[i - 1 - j] = tmp;
+  }
+  p[i] = '\0';
+  return i;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	int len = 0;
+  va_list args;
+  va_start(args, fmt);
+
+  for (const char *f = fmt; *f != '\0'; f++) {
+    if (*f != '%') {
+			putch(*f);
+			len++;
+    } else {
+      f++;  // skip '%'
+      if (*f == 'd') {
+        int val = va_arg(args, int);
+        char buf[20];
+        int l = int2str(val, buf);
+        for (int i = 0; i < l; i++) {
+					putch(buf[i]);
+				}
+				len += l;
+      } else if (*f == 's') {
+        const char *s = va_arg(args, const char *);
+        while (*s) {
+					putch(*s++);
+					len++;
+				}
+      } else if (*f == 'x') {
+        uint32_t val = va_arg(args, uint32_t);
+        char buf[20];
+        int l = x2str(val, buf);
+        for (int i = 0; i < l; i++) {
+					putch(buf[i]);
+				}
+				len += l;
+      } else {
+        putch('%');
+        putch(*f);
+				len++;
+      }
+    }
+  }
+
+  putch('\0');  // 结尾 '\0'
+  va_end(args);
+  return len;  // 返回写入长度
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
