4bc679352e9f9f9ba2794f12600d6db7d042cbe1
shaw <xz_640888@163.com>
2025-09-14 12:57:56 +0800

    keyboard and vga

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,13 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+	uint32_t keycode = inl(KBD_ADDR);
+  kbd->keydown = (keycode & KEYDOWN_MASK) == 0x8000;
+  kbd->keycode = keycode & ~KEYDOWN_MASK;
+  //kbd->keydown = 0;
+  //kbd->keycode = AM_KEY_NONE;
 }
