a0b6354d84205672b986f45e1879bc34f0910479
shaw-wu <xz_640888@163.com>
2026-01-23 11:36:19 +0800

    ysy-E minirvEMU

--- ysyx-E/minirvEMU/cpu.c
+++ ysyx-E/minirvEMU/cpu.c
@@ -1,88 +1,222 @@
 #include<stdint.h>
 #include<stdio.h>
+#include<string.h>
 #include<stdbool.h>
+#include<assert.h>
 
-#define RS1_ST 2
-#define RS1_EN 3
-#define RS2_ST 0
-#define RS2_EN 1
-#define RD_ST 4
-#define RD_EN 5
-#define IMM_ST 0
-#define IMM_EN 3
-#define ADDR_ST 2
-#define ADDR_EN 5
-#define OP_ST 6
-#define OP_EN 7
+#define MEM_SIZE 0xf0000 
 
-#define ADD 0x0
-#define LI 0x2
-#define BNER0 0x3
+#define RS1_ST 15 
+#define RS2_ST 20
+#define RD_ST 7
+#define FUNCT3_ST 12 
+#define FUNCT7_ST 25
+#define OPCODE_ST 0
 
-uint8_t PC = 0;
-uint8_t R[4] = {0x00, 0x00, 0x00, 0x00};
-uint8_t M[16] = {
-        0x8a,/*10001010*/
-        0x90,/*10010000*/
-        0xa0,/*10100000*/
-        0xb1,/*10110001*/
-        0x17,/*00010111*/
-        0x29,/*00101001*/
-        0xd1,/*11010001*/
-        0xdf,/*11011111*/ };
+typedef struct {
+        uint32_t pc,
+        uint32_t gpr[16]
+} cpu;
+        
+
+char *img_file = "/home/shaw/ysyx/ysyx-E/minirvEMU/img/sum.bin";
+char *diff_file = "/home/shaw/ysyx/ysyx-E/minirvEMU/sum_diff";
+cpu ref_cpu;
+FILE *dif = NULL;
+uint32_t PC = 0x00000000;
+uint32_t R[16] = {};
+uint8_t M[MEM_SIZE] = {};
 bool done = false;
+int ebreak = 0;
 
-void decode_exec(uint8_t inst, bool *regwr, uint8_t *wdata, uint8_t *rd, uint8_t *addr){
-        uint8_t src1, src2, imm, opcode;
-        src1 = R[(inst >> RS1_ST) & 0x03];
-        src2 = R[(inst >> RS2_ST) & 0x03];
-        *rd = (uint8_t)((inst >> RD_ST) & 0x03);
-        imm = (uint8_t)((inst >> IMM_ST) & 0x0f);
-        opcode = (uint8_t)((inst >> OP_ST) & 0x03);
-            switch(opcode){
-                    case ADD :
-                            *regwr = true;
-                            *wdata = src1 + src2; 
-                            *addr = PC + 1;
-                            break;
-                    case LI :
-                            *regwr = true;
-                            *wdata = imm; 
-                            *addr = PC + 1;
-                            break;
-                    case BNER0 :
-                            *regwr = false;
-                            *addr = (R[0] != src2) ? (uint8_t)((inst >> ADDR_ST) & 0x0f) : PC + 1;
-                            break;
-                    default : *regwr = false; break;
-            }
-        printf("inst = 0x%02x, src1 = 0x%02x, src2 = 0x%02x, rd = 0x%1x, wdata = 0x%02x, imm = 0x%08x, opcode = 0x%08x\n", inst, src1, src2, *rd, *wdata, imm, opcode);
-        printf("regwr = %s\n", *regwr ? "true" : "false");
+long load_img(){
+      assert(img_file);
+      
+      FILE *fp = fopen(img_file, "rb");
+      assert(fp);
+
+      fseek(fp, 0, SEEK_END);
+      long size = ftell(fp);
+      printf("The image size = %ld\n", size);
+
+      fseek(fp, 0, SEEK_SET);
+      int ret = fread(M, size, 1, fp);
+      assert(ret == 1);
+
+      fclose(fp);
+      return size;
+} 
+
+void read_dif(FILE *fp, char *read){
+    char buf[256];
+    fgets(buf, sizeof(buf), fp); // 读第 1 行
+    memcpy(read, buf, sizeof(buf));
+}
+
+void jmp_fp(FILE* fp, int n){
+    for(int i = 0; i < n; i++){
+        int c;
+        while ((c = fgetc(fp)) != EOF) {
+            if (c == '\n')
+                break;
+        }
+    }
+}
+
+int read_mem(uint32_t *rdata, uint32_t addr, uint8_t len){
+        //for(int i = 0; i < 10; i++){
+        //        printf("M[%d] = 0x%02x\n", addr + i, M[addr + i]);
+        //}
+        *rdata = len == 1 ? (uint32_t)M[addr]        :
+                 len == 2 ? (uint32_t)M[addr] | (uint32_t)(M[addr+1] << 8) :
+                 len == 4 ? (uint32_t)M[addr] | (uint32_t)(M[addr+1] << 8) | (uint32_t)(M[addr+2] << 16) | (uint32_t)(M[addr+3] << 24) : (uint32_t)0;
+        return len > 4;
+} 
+
+int write_mem(uint32_t wdata, uint32_t addr, uint8_t len){
+        if(len == 1){
+                M[addr] = (uint8_t)wdata;
+        } else if(len == 2){
+                M[addr] = (uint8_t)((uint32_t)(wdata << 24) >> 24);
+                M[addr+1] = (uint8_t)((uint32_t)(wdata << 16) >> 24);
+        } else if(len == 4){
+                M[addr  ] = (uint8_t)((uint32_t)(wdata << 24) >> 24);
+                M[addr+1] = (uint8_t)((uint32_t)(wdata << 16) >> 24);
+                M[addr+2] = (uint8_t)((uint32_t)(wdata << 8 ) >> 24);
+                M[addr+3] = (uint8_t)((uint32_t)(wdata      ) >> 24);
+        } else{
+                return 1;
+        }
+        return 0;
+} 
+      
+
+void decode_exec(uint32_t inst, bool *regwr, bool *memwr, uint32_t *reg_wdata, uint32_t *mem_wdata, uint32_t *mem_waddr, uint8_t *len, uint32_t *rd, uint32_t *npc){
+        uint32_t rdata = 0;
+        uint32_t src1, src2, imm;
+        uint32_t opcode, funct3, funct7;
+        src1 = R[(inst >> RS1_ST) & 0x0000001f];
+        src2 = R[(inst >> RS2_ST) & 0x0000001f];
+        *rd = (uint32_t)((inst >> RD_ST) & 0x0000001f);
+        opcode = (uint32_t)((inst >> OPCODE_ST) & 0x0000007f);
+        funct3 = (uint32_t)((inst >> FUNCT3_ST) & 0x00000007);
+        funct7 = (uint32_t)((inst >> FUNCT7_ST) & 0x0000007f);
+        bool add, addi, lui, lw, lbu, sb, sw, jalr = false;
+        if(opcode == 0b0110011 && funct3 == 0b000 && funct7 == 0b0000000){
+                add = true;
+                imm = 0; 
+                *regwr = true;
+                *memwr = false;
+                *reg_wdata = src1 + src2;
+                *mem_wdata = 0;
+                *npc = PC + 4;
+        } else if(opcode == 0b0010011 && funct3 == 0b000){
+                addi = true;
+                imm = (uint32_t)((inst >> 20) & 0x00000fff);
+                *regwr = true;
+                *memwr = false;
+                *reg_wdata = src1 + imm;
+                *mem_wdata = 0;
+                *npc = PC + 4;
+        } else if(opcode == 0b0110111){
+                lui = true;
+                imm = (uint32_t)(inst & 0xfffff000);
+                *regwr = true;
+                *memwr = false;
+                *reg_wdata = imm;
+                *mem_wdata = 0;
+                *npc = PC + 4;
+        } else if(opcode == 0b0000011 && funct3 == 0b010){
+                lw = true;
+                imm = (uint32_t)((inst >> 20) & 0x00000fff);
+                *regwr = true;
+                *memwr = false;
+                read_mem(&rdata, src1 + imm, 4);
+                *reg_wdata = rdata;
+                *mem_wdata = 0;
+                *npc = PC + 4;
+        } else if(opcode == 0b0000011 && funct3 == 0b100){
+                lbu = true;
+                imm = (uint32_t)((inst >> 20) & 0x00000fff);
+                *regwr = true;
+                *memwr = false;
+                read_mem(&rdata, src1 + imm, 1);
+                *reg_wdata = rdata;
+                *mem_wdata = 0;
+                *npc = PC + 4;
+        } else if(opcode == 0b0100011 && funct3 == 0b000){
+                sb = true;
+                imm = (uint32_t)(((inst >> 6) & 0x0000001f) | ((inst >> 20) & 0x0000007f));
+                *regwr = false;
+                *memwr = true;
+                *mem_waddr = imm + src1;
+                *len = 1;
+                *reg_wdata = 0;
+                *mem_wdata = (uint32_t)((src2 << 24) >> 24);
+                *npc = PC + 4;
+        } else if(opcode == 0b0100011 && funct3 == 0b010){
+                sw = true;
+                imm = (uint32_t)(((inst >> 6) & 0x0000001f) | ((inst >> 20) & 0x0000007f));
+                *regwr = false;
+                *memwr = true;
+                *mem_waddr = imm + src1;
+                *len = 4;
+                *reg_wdata = 0;
+                *mem_wdata = src2;
+                *npc = PC + 4;
+        } else if(opcode == 0b1100111 && funct3 == 0b000){
+                jalr= true;
+                imm = (uint32_t)((inst >> 20) & 0x0000000fff);
+                *regwr = true;
+                *memwr = false;
+                *reg_wdata = PC + 4;
+                *mem_wdata = 0;
+                *npc = (src1 + imm) & 0xfffffffe;
+        } else if(inst == 0x00100073){
+                ebreak = 1;
+                *regwr = false;
+                *memwr = false;
+        } else{
+                printf("undecode inst : 0x%08x\n", inst);
+                assert(0);
+                return;
+        }
+        //printf("inst = 0x%02x, src1 = 0x%02x, src2 = 0x%02x, rd = 0x%1x, wdata = 0x%02x, imm = 0x%08x, opcode = 0x%08x\n", inst, src1, src2, *rd, *wdata, imm, opcode);
+        //printf("regwr = %s\n", *regwr ? "true" : "false");
        return;
 }
 
 void inst_cycle(){
-        bool regwr;
-        uint8_t wdata, addr, rd;
-        decode_exec(M[PC], &regwr, &wdata, &rd, &addr);
-        if(regwr) R[rd] = wdata;
-        if(M[PC] == 0xdf) done = true ;
-        PC = addr;
+        bool regwr, memwr;
+        uint32_t reg_wdata, mem_wdata, mem_waddr, npc, rd;
+        uint32_t inst;
+        uint8_t len;
+        read_mem(&inst, PC, 4); 
+        printf("pc = 0x%08x, inst = 0x%08x\n", PC, inst);
+        decode_exec(inst, &regwr, &memwr, &reg_wdata, &mem_wdata, &mem_waddr, &len, &rd, &npc);
+        if(regwr) R[rd] = reg_wdata;
+        R[0] = 0;
+        if(memwr) write_mem(mem_wdata, mem_waddr, len);
+        if(ebreak) done = true ;
+        PC = npc;
 }
 
 void R_out(){
-        for(int i = 0; i < 4; i++){
-                printf("R[%d] = 0x%02x   ", i, R[i]);
+        for(int i = 0; i < 16; i++){
+                printf("R[%d] = 0x%08x   ", i, R[i]);
         }
         printf("\n");
 }
 
 int main(){
+        memset(M, 0, sizeof(M));
+        memset(R, 0, sizeof(R));
+        load_img();
         while(1){
-                printf("=============================\n");
+                //printf("=============================\n");
                 inst_cycle();
-                R_out();
-                printf("done = %s\n", done? "true" : "false");
+                //R_out();
+                if(done) printf("done\n");
                 if(done) break;
                 //break;
         }
--- ysyx-E/sEMU/cpu.c
+++ ysyx-E/sEMU/cpu.c
@@ -16,6 +16,7 @@
 #define OP_EN 7
 
 #define ADD 0x0
+#define OUT 0x1
 #define LI 0x2
 #define BNER0 0x3
 
@@ -29,10 +30,11 @@ uint8_t M[16] = {
         0x17,/*00010111*/
         0x29,/*00101001*/
         0xd1,/*11010001*/
+        0x48,/*01001000*/
         0xdf,/*11011111*/ };
 bool done = false;
 
-void decode_exec(uint8_t inst, bool *regwr, uint8_t *wdata, uint8_t *rd, uint8_t *addr){
+void decode_exec(uint8_t inst, bool *regwr, bool *memwr, uint8_t *wdata, uint8_t *mem_wdata, uint8_t *rd, uint8_t *addr){
         uint8_t src1, src2, imm, opcode;
         src1 = R[(inst >> RS1_ST) & 0x03];
         src2 = R[(inst >> RS2_ST) & 0x03];
@@ -42,29 +44,39 @@ void decode_exec(uint8_t inst, bool *regwr, uint8_t *wdata, uint8_t *rd, uint8_t
             switch(opcode){
                     case ADD :
                             *regwr = true;
+                            *memwr = false;
                             *wdata = src1 + src2; 
+                            *addr = PC + 1;
+                            break;
+                    case OUT :
+                            *regwr = false;
+                            *memwr = true;
+                            *mem_wdata = src1;
                             *addr = PC + 1;
                             break;
                     case LI :
                             *regwr = true;
+                            *memwr = false;
                             *wdata = imm; 
                             *addr = PC + 1;
                             break;
                     case BNER0 :
                             *regwr = false;
+                            *memwr = false;
                             *addr = (R[0] != src2) ? (uint8_t)((inst >> ADDR_ST) & 0x0f) : PC + 1;
                             break;
                     default : *regwr = false; break;
             }
-        printf("inst = 0x%02x, src1 = 0x%02x, src2 = 0x%02x, rd = 0x%1x, wdata = 0x%02x, imm = 0x%08x, opcode = 0x%08x\n", inst, src1, src2, *rd, *wdata, imm, opcode);
-        printf("regwr = %s\n", *regwr ? "true" : "false");
+        //printf("inst = 0x%02x, src1 = 0x%02x, src2 = 0x%02x, rd = 0x%1x, wdata = 0x%02x, imm = 0x%08x, opcode = 0x%08x\n", inst, src1, src2, *rd, *wdata, imm, opcode);
+        //printf("regwr = %s\n", *regwr ? "true" : "false");
        return;
 }
 
 void inst_cycle(){
-        bool regwr;
-        uint8_t wdata, addr, rd;
-        decode_exec(M[PC], &regwr, &wdata, &rd, &addr);
+        bool regwr, memwr;
+        uint8_t wdata, mem_wdata, addr, rd;
+        decode_exec(M[PC], &regwr, &memwr, &wdata, &mem_wdata, &rd, &addr);
+        if(memwr) printf("0x%02x\n", mem_wdata);
         if(regwr) R[rd] = wdata;
         if(M[PC] == 0xdf) done = true ;
         PC = addr;
@@ -79,10 +91,10 @@ void R_out(){
 
 int main(){
         while(1){
-                printf("=============================\n");
+                //printf("=============================\n");
                 inst_cycle();
-                R_out();
-                printf("done = %s\n", done? "true" : "false");
+                //R_out();
+                //printf("done = %s\n", done? "true" : "false");
                 if(done) break;
                 //break;
         }
